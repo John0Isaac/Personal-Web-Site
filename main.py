45012adeded1b5c2b0a9171c1d43ea1a4d9514c5
John0Isaac <johnaziz269@gmail.com>
2021-05-03 00:41:25 +0200

    WhiteSpaces

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from flask import Flask
 
+
 def create_app(test_config=None):
     # Create and configure the app
     app = Flask(__name__)
-    
+
     @app.route('/')
     def landing_page():
-      return "Hello World"
-    
+        return "Hello World"
+
     return app
+
+
 app = create_app()
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run()
