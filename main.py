c78b0538516c047a9661edc1b6b182991c05591f
John0Isaac <johnaziz269@gmail.com>
2021-05-14 18:12:51 +0200

    CORS, Linking frontend

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-from flask import Flask
-
+from flask import Flask, request, render_template
+from flask_cors import CORS
 
 def create_app(test_config=None):
     # Create and configure the app
     app = Flask(__name__)
+    CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+    # CORS Headers
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization,true')
+        response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
+        return response
 
     @app.route("/")
     def landing_page():
-        return "Hello World"
+        return render_template('pages/index.html')
 
     return app
 
